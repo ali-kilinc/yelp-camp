b6c58a9bbac61cd5d262237681d15d81029004bd
ali-kilinc <alikilinc1986@gmail.com>
2020-06-30 14:20:20 +0300

    Remove unsupported character for internet exp

--- public/scripts/campgrounds/campgroundForm.js
+++ public/scripts/campgrounds/campgroundForm.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
         if (searchText) {
             const options = {
                 method: "GET",
-                url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?access_token=${mapboxPublicToken}`,
+                url: "https://api.mapbox.com/geocoding/v5/mapbox.places/" + searchText + ".json?access_token=" + mapboxPublicToken,
                 cache: false
             };
 
